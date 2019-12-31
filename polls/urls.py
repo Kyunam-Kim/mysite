e2cbcf35d43c205d0c3a9641d14da4d81ea9f89e
kim-kau-nam <rlarbska3824@gmail.com>
2019-12-31 23:08:14 +0900

    fourth commit

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
   path('',views.index, name = 'index'),
   path('<int:question_id>/', views.detail, name = 'detail'),
   path('<int:question_id>/results/', views.results, name = 'results'),
   path('<int:question_id>/vote/', views.vote, name = 'vote'),
+  # 해당 url에 view의 vote를 호출
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question
 from django.template import loader
 from django.shortcuts import render
 from django.http import Http404
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -22,4 +23,16 @@ def results(request, question_id):
 	return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+		# 템플릿에서 넘겨받은 값을 조회 request.POST['이름 or name']
+	except (KeyError, ChoiceDoesNotExist):
+		return render(request, 'polls/detail.html', {
+			'question' : question,
+			'error_message' : "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
