476320e46b46934d12d0991d5ad302963bededae
kim-kau-nam <rlarbska3824@gmail.com>
2020-01-11 23:52:16 +0900

    modified

--- polls/admin.py
+++ polls/admin.py
@@ -4,9 +4,11 @@ from .models import Question, Choice
 # Register your models here.
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date','question_text']
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information', {'fields':['pub_date']}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Question)
 admin.site.register(Choice)
